4594a9c6d88f1f15595f222602a33bcdc5df5dcb
Andrew Siegman <blerrnk@gmail.com>
2017-10-28 02:29:43 -0500

    fixes

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"log"
 	"net"
+	// "github.com/blunket/tic-tac-go/game"
 )
 
 const (
@@ -14,27 +16,31 @@ const (
 func main() {
 
 	ln, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
+	defer ln.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer ln.Close()
 	log.Printf("Listening on %s:%s", CONN_HOST, CONN_PORT)
+
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
-			log.Fatal(err)
+			break
 		}
 		go handleConn(conn)
 	}
 }
 
 func handleConn(conn net.Conn) {
-	buf := make([]byte, 1024)
 
-	_, err := conn.Read(buf)
-	if err != nil {
-		log.Fatal(err)
+	defer conn.Close()
+	for {
+		msg, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			break
+		}
+		conn.Write([]byte("Received message: " + msg + "\n"))
+		log.Println(msg)
 	}
-	conn.Write([]byte("Received message."))
-	conn.Close()
+
 }
