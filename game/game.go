5a51e2df39a9d502dabac7bdc8ce7ff015079a14
Andrew Siegman <blerrnk@gmail.com>
2017-11-01 01:16:17 -0500

    update game pkg

--- game/game.go
+++ game/game.go
@@ -3,11 +3,14 @@ package game
 
 import (
 	"github.com/blunket/tic-tac-go/game/grid"
+	"sync"
 )
 
+// Game is a Tic-Tac-Toe game controller.
 type Game struct {
-	Board grid.Grid
-	Turn  int
+	board grid.Grid
+	turn  int // Turn identifies whose turn it is. 1 or 2.
+	sync.Mutex
 }
 
 // New creates a new Game, and the provided player goes first.
@@ -17,15 +20,23 @@ func New(p int) Game {
 	}
 }
 
+// Board returns the board state as a slice.
+func (g *Game) Board() [9]int {
+	return g.board.Slice()
+}
+
 // Place sets a square at x,y on a grid to p if the square is open.
 // It must be the player's turn.
 // Returns whether successful.
 func (g *Game) Place(p, x, y int) bool {
+	g.Lock()
+	defer g.Unlock()
+
 	if g.Turn != p {
 		return false
 	}
 
-	sq := g.Board.Place(p, x, y)
+	sq := g.board.Place(p, x, y)
 	if sq == false {
 		return false
 	}
