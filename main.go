7aa2e7bcef5e6328dc041247fbf2b592d67aaf14
Andrew Siegman <blerrnk@gmail.com>
2017-11-01 01:41:50 -0500

    more testing

--- main.go
+++ main.go
@@ -18,17 +18,17 @@ func main() {
 
 	r := httprouter.New()
 
-	r.GET("/new", NewGame)
-	r.GET("/grid", Grid)
+	r.GET("/new", newGame)
+	r.GET("/game/:gameId/grid", grid)
 
 	log.Printf("Listening on %s:%s\n", connHost, connPort)
 	log.Fatalln(http.ListenAndServe(connHost+":"+connPort, r))
 
 }
 
-func grid(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "Hello, world!")
-	log.Println("/grid")
+func grid(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "Game: %s", ps.ByName("gameId"))
+	log.Println("grid: ")
 }
 
 func newGame(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
